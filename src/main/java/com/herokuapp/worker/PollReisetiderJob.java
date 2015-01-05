aa415731e3fcf1df56523d0daa9b58cb4e761c26
Knut H Pedersen <knuthp@gmail.com>
2015-01-05 21:26:33 +0100

    Changed to use apache commons-io to access HTTP.

--- src/main/java/com/herokuapp/worker/PollReisetiderJob.java
+++ src/main/java/com/herokuapp/worker/PollReisetiderJob.java
@@ -1,13 +1,11 @@
 package com.herokuapp.worker;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.commons.io.IOUtils;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -15,38 +13,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PollReisetiderJob implements Job {
-	private final String USER_AGENT = "Mozilla/5.0";
-	final static Logger logger = LoggerFactory.getLogger(PollReisetiderJob.class);
+	final static Logger logger = LoggerFactory
+			.getLogger(PollReisetiderJob.class);
 
 	public void execute(JobExecutionContext jobExecutionContext)
 			throws JobExecutionException {
+		String url = "http://spartid-server.herokuapp.com/";
+
 		try {
-			String url = "http://spartid-server.herokuapp.com/";
-
-			HttpClient client = new DefaultHttpClient();
-			HttpGet request = new HttpGet(url);
-
-			// add request header
-			request.addHeader("User-Agent", USER_AGENT);
-
-			HttpResponse response = client.execute(request);
-
-			logger.info("\nSending 'GET' request to URL : " + url);
-			logger.info("Response Code : "
-					+ response.getStatusLine().getStatusCode());
-
-			BufferedReader rd = new BufferedReader(new InputStreamReader(
-					response.getEntity().getContent()));
-
-			StringBuffer result = new StringBuffer();
-			String line = "";
-			while ((line = rd.readLine()) != null) {
-				result.append(line);
-			}
-
-			logger.info(result.toString());
+			InputStream in = new URL(url).openStream();
+			IOUtils.toString(in);
+			logger.info("Polled: " + url);
+		} catch (MalformedURLException e) {
+			logger.error("Trouble with url: " + url, e);
 		} catch (IOException e) {
-			logger.error("Got exection", e);
+			logger.error("Trouble with IO: " + url, e);
 		}
 	}
 
